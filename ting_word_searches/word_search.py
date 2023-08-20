6e4b1447a87d1368ee26d44d8a000bbc501cd08d
joao <joao.dev94@gmail.com>
2023-08-20 20:37:12 -0300

    Requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file_info in instance.queue:
+        file_occurrences = []
+        line_number = 0
+
+        for line_content in file_info['linhas_do_arquivo']:
+            line_number += 1
+
+            if word.lower() in line_content.lower():
+                file_occurrences.append({'linha': line_number})
+
+        if file_occurrences:
+            result.append({
+                'palavra': word,
+                'arquivo': file_info['nome_do_arquivo'],
+                'ocorrencias': file_occurrences
+            })
+
+    return result
+
 
 
 def search_by_word(word, instance):
