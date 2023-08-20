ef59bfaeb5dbacdb37018091ef16c0ddd1882307
joao <joao.dev94@gmail.com>
2023-08-20 19:41:27 -0300

    Requisito 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,20 @@
-# from ting_file_management.priority_queue import PriorityQueue
-
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    files = [
+        {"nome_do_arquivo": "file1.txt", "qtd_linhas": 3},
+        {"nome_do_arquivo": "file2.txt", "qtd_linhas": 8},
+        {"nome_do_arquivo": "file3.txt", "qtd_linhas": 2},
+        {"nome_do_arquivo": "file4.txt", "qtd_linhas": 7},
+    ]
+
+    for file in files:
+        priority_queue.enqueue(file)
+
+    assert priority_queue.dequeue() == {"nome_do_arquivo": "file1.txt", "qtd_linhas": 3}
+    assert priority_queue.search(0) == {"nome_do_arquivo": "file3.txt", "qtd_linhas": 2}
+    with pytest.raises(IndexError):
+        priority_queue.search(100)
